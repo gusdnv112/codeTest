88c6cdf22a1b30c5b5cf09efd00a4ae7a361fe10
Kim bo sung <yhi002521@gmail.com>
2021-03-16 19:12:56 +0900

    programmers LV2

---
+++ src/CountryNum124.java
@@ -0,0 +1,15 @@
+public class CountryNum124 {
+    public static void main(String[] args) {
+        System.out.print(solution(4));
+    }
+    public static String solution(int n) {
+        String answer = new String("");
+        int addNum = 0;
+        while (n != 0) {
+            addNum = n % 3 == 0 ? 4 : n%3;
+            answer = Integer.toString(addNum) + answer;
+            n = addNum == 4 ? n/3 - 1 : n/3;
+        }
+        return answer;
+    }
+}
---
+++ src/StockPrice.java
@@ -0,0 +1,30 @@
+import java.util.Stack;
+public class StockPrice {
+    public static void main(String[] args) {
+        int[] prices = {1, 2, 3, 2, 3};
+        int[] answerArray = solution(prices);
+        for (int i : answerArray) {
+            System.out.print(i + " ");
+        }
+    }
+    public static int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        int count = 0;
+
+        for(int i=0; i<prices.length-1; i++) {
+            count = 0;
+            for(int j=i+1; j<prices.length; j++) {
+                if(prices[i]<=prices[j]) {
+                    count++;
+                } else {
+                    count++;
+                    break;
+                }
+            }
+            answer[i] = count;
+        }
+        answer[prices.length-1] = 0;
+
+        return answer;
+    }
+}
