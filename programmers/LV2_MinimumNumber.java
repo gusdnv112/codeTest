95946b521f3cc5a4cd3c5caeefea1643433c4276
dalpeng <gusdnv112@gmail.com>
2021-03-23 23:54:46 +0900

    remove unused param

--- programmers/LV2_MinimumNumber.java
+++ programmers/LV2_MinimumNumber.java
@@ -8,19 +8,15 @@ public class LV2_MinimumNumber {
 	public static int solution(int []A, int []B)
     {
         int answer = 0;
-        int min;
-        int tmp =0;
 
         Arrays.sort(A);
         Arrays.sort(B);
         
         
         for(int i=0; i < A.length; i++) {
-        	tmp = tmp + A[i] * B[B.length-i-1];
+        	answer = answer + A[i] * B[B.length-i-1];
         }
-        answer = tmp;
-	    System.out.println(answer);
-        
+       
         return answer;
     }
 	
