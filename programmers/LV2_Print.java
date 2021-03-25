86fc94a3e35fce31eee51e44add6369a8f3ae565
dalpeng <gusdnv112@gmail.com>
2021-03-26 02:16:31 +0900

    3/26

---
+++ programmers/LV2_Print.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class LV2_Print {
+	
+	public static int solution(int[] priorities, int location) {
+		int answer = 0;
+	    
+		return answer;
+	 }
+	 
+	 public static void main(String[] args) {
+		 
+	}
+	 
+}
